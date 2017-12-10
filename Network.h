9976479626e3d2708cc5db292f290b9fb6aec783
destroyedlolo <l_faillie@yahoo.com>
2017-12-10 18:50:30 +0100

    Add network handling

--- Network.h
+++ Network.h
@@ -11,25 +11,130 @@
 	 *****/
 class Network {
 	enum NetworkMode { 
-		SAFEDM, 	// Try Automation network first then home one
-		SAFEMD,		// Try Home network first then automation one
-		MAISON,		// Only one home network
-		DOMOTIQUE	// Only one Automation network
-	} mode;	// which mode is in use
-	bool first;	// first connection attempt
+		FAILURE = 0,	// In case of no network can be reached
+		SAFEDM, 		// Try Automation network first then home one
+		SAFEMD,			// Try Home network first then automation one
+		MAISON,			// Only one home network
+		DOMOTIQUE		// Only one Automation network
+	} mode, current;	// which mode is in use
+	unsigned int attempts;	// If in degraded mode, counter before recovery try
 
 protected:
+	virtual void save( void ) = 0;	// Needed to call context's one
+
 	void init( void ){ /* Initial configuration */
-		this->mode = NetworkMode::MAISON;
-		this->first = true;
+		this->mode = NetworkMode::SAFEDM;
+		this->current = this->getNominalNetwork();
+	}
+
+		/***
+		 * Notez-Bien : settings are all externally defined
+		 ***/
+	bool connectMaison( bool persistant = true ){
+		Serial.println("Connecting to home network");
+
+		if( !persistant )
+			WiFi.persistent(false);
+
+#	if STATIC_IP
+		WiFi.config(adr_ip, adr_gateway, adr_dns);
+#	endif
+		WiFi.begin( WIFI_SSID, WIFI_PASSWORD );
+		for( int i=0; i< 240; i++ ){
+			if(WiFi.status() == WL_CONNECTED){
+				if( !persistant )
+					WiFi.persistent(true);
+				return true;
+			}
+			delay(500);
+			Serial.print(".");
+		}
+		return false;
+	}
+
+	bool connectDomotique( bool persistant = true ){
+		Serial.println("Connecting to Domotique network");
+
+		if( !persistant )
+			WiFi.persistent(false);
+
+		WiFi.begin( DOMO_SSID, DOMO_PASSWORD );
+		for( int i=0; i< 240; i++ ){
+			if(WiFi.status() == WL_CONNECTED){
+				if( !persistant )
+					WiFi.persistent(true);
+				return true;
+			}
+			delay(500);
+			Serial.print("-");
+		}
+		return false;
+	}
+
+	enum NetworkMode getNominalNetwork( void ){	// Where to connect normally
+		switch( this->mode ){
+		case NetworkMode::SAFEDM :
+		case NetworkMode::DOMOTIQUE :
+			return NetworkMode::DOMOTIQUE;
+		default:
+			return NetworkMode::MAISON;
+		}
+	}
+
+	enum NetworkMode getAlternateNetwork( void ){	// Where to connect alternatively
+		switch( this->mode ){
+		case NetworkMode::SAFEDM :
+			return NetworkMode::MAISON;
+		case NetworkMode::SAFEMD :
+			return NetworkMode::DOMOTIQUE;
+		default:
+			return NetworkMode::FAILURE;
+		}
 	}
 
 public:
-	bool isfirstrun( void ){ return this->first; }
-	void hasrun( void ){ this->first = false; }
+	bool isDegraded( void ){
+		return( this->getNominalNetwork() != this->current );
+	}
 	
 	void setMode( enum NetworkMode n ){ this->mode = n; }
 	enum NetworkMode getMode( void ){ return this->mode; }
+
+	enum NetworkMode connect( void ){
+		if( this->isDegraded() ){
+			if( ! --this->attempts )	// Back to nominal network
+				this->current = this->getNominalNetwork();
+//			this->save();
+		}
+
+		if( this->current == NetworkMode::MAISON ){
+			if( !this->connectMaison() ){
+				if( this->getAlternateNetwork() == NetworkMode::DOMOTIQUE && // we were on the nominal network
+				this->connectDomotique(false)){	// And we successfully connect to the alternate one
+					this->attempts = RETRYAFTERSWITCHING;
+					this->current = NetworkMode::DOMOTIQUE;
+				} else {	// Failing
+					this->attempts = RETRYAFTERFAILURE;
+					this->current = NetworkMode::FAILURE;
+				}
+			}
+		} else if( this->current == NetworkMode::DOMOTIQUE ){
+			if( !this->connectDomotique() ){
+				if( this->getAlternateNetwork() == NetworkMode::MAISON && // we were on the nominal network
+				this->connectMaison(false)){	// And we successfully connect to the alternate one
+					this->attempts = RETRYAFTERSWITCHING;
+					this->current = NetworkMode::MAISON;
+					this->save();
+				} else {	// Failing
+					this->attempts = RETRYAFTERFAILURE;
+					this->current = NetworkMode::FAILURE;
+				}
+			}
+		}
+
+		this->save();
+		return(this->current);
+	}
 };
 
 #endif
