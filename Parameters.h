66a38ac3458e6b6dc2fbf02dd8bb476e792eb0c9
destroyedlolo <l_faillie@yahoo.com>
2019-05-12 20:16:06 +0200

    WIP

--- Parameters.h
+++ Parameters.h
@@ -10,57 +10,48 @@
 	 *	Comment or uncomment to activate or disable some optional parts.
 	 ******/
 
-#	define DEV_ONLY	/* Include developpment only code */
-/*#define STATIC_IP*/	/* Use static IP when on home network */
-/*#define LED_BUILTIN 2*/	/* Workaround for my ESP-12 */
+#define DEV	// Development mode
 
-	/* If enabled, LED is lighting during network establishment 
-	 * NOTEZ-BIEN : on ESP-201 this is mutually exclusif vs serial output
+#ifndef DEV
+
+	/****
+	 * Production settings
+	 ****/
+
+#	define MQTT_CLIENT "Poulailler"
+
+	/* LED brights during network and MQTT acquisition
 	 */
-#if 0		// Led is lightning during Wifi / Mqtt connection establishment
 #	define LED(x)	{ digitalWrite(LED_BUILTIN, x); }
+#	define DEF_NESTSLEEP 300	// Default sleep b/w nest sample (in seconds)
+#	define DEF_VERBOSITY false	// low level of information send (by default, no to save some energies)
+
 #else
+
+	/****
+	 * Development settings
+	 ****/
+
+#	define MQTT_CLIENT "Poulailler-Dev"
+
+	/* Debug message on tx
+	 * Notez-bien : LED disabled as on the same GPIO
+	 */
 #	define LED(x)	{ }
 #	define SERIAL_ENABLED
+
+#	define DEF_NESTSLEEP 30		// Sleep b/w nest sample (short as we're debugging)
+#	define DEF_VERBOSITY true	// Hight level of information send
 #endif
 
-#ifdef STATIC_IP
-	/* Static IP to avoid DHCP querying delay */
-IPAddress adr_ip(192, 168, 0, 17);
-IPAddress adr_gateway(192, 168, 0, 10);
-IPAddress adr_dns(192, 168, 0, 3);
-#endif
+	/* MQTT */
+String MQTT_Topic(MQTT_CLIENT);	// Topic's root
+String MQTT_Error = MQTT_Topic + "/Error";
+String MQTT_Command = MQTT_Topic + "/Command";
 
 	/* GPIOs */
 #define AUXPWR_GPIO	0 /* Power auxiliaries, on prod 0 */
 #define ONE_WIRE_BUS 2 // Where OW bus is connected to
 #define pinDHT 5	// Where DHT is connected to
-
-
-	/* MQTT */
-#define MQTT_CLIENT "Poulailler"
-String MQTT_Topic("Poulailler/");	// Topic's root
-String MQTT_Error = MQTT_Topic + "Error";
-String MQTT_Output = MQTT_Topic + "Message";
-String MQTT_Command = MQTT_Topic + "Command";
-
-	/* Delays */
-#define DELAY_STARTUP	5		// Let a chance to enter in interactive mode at startup ( 5s )
-#define DELAY_LIGHT 500			// Delay during light sleep (in ms - 0.5s )
-
-#define INTERVAL_DEVICE 300		// Interval b/w samples for the device in S (5 minutes)
-#define INTERVAL_PERCHOIR 300	// Interval b/w samples for the "perchoir" in S (5 minutes)
-
-#define INTERVAL_AUX 900		// Interval b/w samples for "auxiliaires" in S (15 minutes)
-#define DELAY_AUX 250			// Delay before light detector capacitor is loaded (in ms)
-
-#define TIMEOUT_DOOR	5000	// Timeout for the door to reach stops in mS
-
-	// Network related delays
-	// Caution to respect delays if in interactive mode
-#define FAILUREDELAY	900		// Delay b/w 2 network attempt in case of failure (15 minutes)
-#define RETRYAFTERSWITCHING	12	// number of connections before trying the nominal network in case of degraded mode
-#define RETRYAFTERFAILURE	3	// number of connections before trying the nominal network in case of faillure
-#define MQTTRETRY	60			// Delay b/w MQTT retries (seconds)
 
 #endif
--- Poulailler.ino
+++ Poulailler.ino
@@ -24,13 +24,60 @@ extern "C" {
 #include <Maison.h>		// My very own environment (WIFI_*, MQTT_*, ...)
 #include "Parameters.h"
 
+#include <LFUtilities.h>	// Fake but needed to load LFUtilities
+#include <LFUtilities/Duration.h>	// Duration measurement 
+#include <LFUtilities/SafeMQTTClient.h>	// Network connection
+
+	/****
+	* Configuration management
+	*****/
+#include <KeepInRTC.h>
+KeepInRTC kir;	// RTC memory management
+
+#include "Context.h"
+Context ctx(kir);
+
+#include <LFUtilities/TemporalConsign.h>
+TemporalConsign delaySampleNest(kir);	// Delay b/w 2 nest sample.
+
+	/***
+	* Let's go
+	****/
+WiFiClient clientWiFi;
+SafeMQTTClient sMQTTc(
+		clientWiFi,
+#ifdef DEV
+		WIFI_SSID, WIFI_PASSWORD,	// Connect to my own home network during development phase
+#else
+		DOMO_SSID, DOMO_PASSWORD,	// "Smart home" network for production
+#endif
+		BROKER_HOST, BROKER_PORT, MQTT_CLIENT,	// Broker's
+		MQTT_CLIENT,	// Messages' root
+		false	// Doesn't clean waiting messages
+);
+
 void setup(){
 #ifdef SERIAL_ENABLED
 	Serial.begin(115200);
 	delay(100);
+
+	Serial.println("Hello");
 #else
 	pinMode(LED_BUILTIN, OUTPUT);
 #endif
+
+	if( ctx.begin() | delaySampleNest.begin(DEF_NESTSLEEP) /*| ctx.begin() */){	// single non logical or, otherwise other begin() will never be called
+#ifdef SERIAL_ENABLED
+		Serial.println("Default value");
+#endif
+	}
+
+	if( !sMQTTc.connectWiFi() ){
+#ifdef SERIAL_ENABLED
+		Serial.println("Unable to connect to the network ... will be back !");
+#endif
+		ctx.deepSleep( delaySampleNest.getConsign() );
+	}
 }
 
 void loop(){
